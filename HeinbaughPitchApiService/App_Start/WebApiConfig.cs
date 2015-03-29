9c13c85730afff8c9eead7978bee7cc355708e08
Nick Heinbaugh <nick.heinbaugh@live.com>
2015-03-28 18:19:39 -0700

    Added nlog

--- HeinbaughPitchApiService/App_Start/WebApiConfig.cs
+++ HeinbaughPitchApiService/App_Start/WebApiConfig.cs
@@ -33,18 +33,18 @@ namespace HeinbaughPitchApiService
     {
         protected override void Seed(HeinbaughPitchApiContext context)
         {
-            List<TodoItem> todoItems = new List<TodoItem>
-            {
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
-            };
+            //List<TodoItem> todoItems = new List<TodoItem>
+            //{
+            //    new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+            //    new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            //};
 
-            foreach (TodoItem todoItem in todoItems)
-            {
-                context.Set<TodoItem>().Add(todoItem);
-            }
+            //foreach (TodoItem todoItem in todoItems)
+            //{
+            //    context.Set<TodoItem>().Add(todoItem);
+            //}
 
-            base.Seed(context);
+            //base.Seed(context);
         }
     }
 }
